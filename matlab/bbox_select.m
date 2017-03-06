b849020ab0a988ebebaf42b0c19797747d7894e7
Zhefan Ye <zhefanye@gmail.com>
2017-03-06 01:54:12 -0500

    update bbox_select

--- matlab/bbox_select.m
+++ matlab/bbox_select.m
@@ -4,13 +4,14 @@ close all
 clc
 
 if nargin < 5
-    obj_list = {'spray_bottle_a', 'detergent', 'clorox'};
-    img_path = '/home/zhefanye/Documents/datasets/ProgressLab/img/sdc_10/';
+    obj_list = {'downy', 'sugar', 'red_bowl'};
+    img_path = '/home/zhefanye/Documents/datasets/ProgressLab/img/dsr_290/';
     ext = 'png';
     img_w = 640;
     img_h = 360;
 end
 
+dataset_img = '/home/zhefanye/Documents/datasets/ProgressLab/progress/JPEGImages/';
 tracked_objs = cell(length(obj_list),1);
 
 for i = 1:length(obj_list)
@@ -27,6 +28,10 @@ end
 if strcmp(str, 'y')
     fprintf('Writing annotations...\n')
     gen_xml(tracked_objs, img_path, img_w, img_h);
+    
+    command = sprintf('cp %s*.%s %s', img_path, ext, dataset_img);
+    system(command,'-echo');
+    fprintf('Copying images...\n')    
 end
 
 end
